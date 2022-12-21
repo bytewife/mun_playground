d306403b1ad8fa45e13c1ae6e3634ded612c5f9b
Ivy <ivy.ember.raine@gmail.com>
2022-12-21 12:59:49 -0800

    feat: add basic hot-reload example

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use mun_runtime::Runtime;
+
+extern "C" fn random() -> i64 {
+    let result = std::time::Instant::now().elapsed().subsec_nanos() as i64;
+    println!("random: {}", result);
+    result
+}
+
+fn main() {
+    // Safety: We assume that the library that is loaded is a valid munlib
+    let builder =
+        Runtime::builder("mod.munlib").insert_fn("random", random as extern "C" fn() -> i64);
+    let mut runtime = unsafe { builder.finish() }.expect("Failed to spawn Runtime");
+
+    let result: bool = runtime.invoke("random_bool", ()).unwrap();
+    println!("random_bool: {}", result);
+    loop {
+        unsafe { runtime.update() };
+        let hotreload_var: i32 = runtime.invoke("hotreload_fn", ()).unwrap();
+        println!("{:?}", hotreload_var);
+    }
+}
